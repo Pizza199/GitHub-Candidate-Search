3b789f0fc72730ccc279aa0d4c4fd8de77457dd7
Pizza199 <steveha999@gmail.com>
2025-03-25 10:58:23 -0500

    candidate search and interface

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,33 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [users, setUsers] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    searchGithub().then((data) => {
+      setUsers(data.items);
+    });
+  }, []);
+  return (
+    <div>
+      <h1>Candidate Search</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login}>
+            <img src={user.avatar_url} alt={user.login} />
+            <a href={user.html_url}>{user.login}</a>
+            <p>{user.name}</p>
+            <p>{user.company}</p>
+            <p>{user.location}</p>
+            <p>{user.email}</p>
+            <p>{user.bio}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CandidateSearch;
