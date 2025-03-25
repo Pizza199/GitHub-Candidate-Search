cf056e4326b8dc0f7d0f6e0166a78b137ffd3d9b
Pizza199 <steveha999@gmail.com>
2025-03-25 14:55:50 -0500

    completed candidate search

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li className="nav-item">
+          <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to="/">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to="/SavedCandidates">Potential Candidates</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,31 +1,65 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 import { Candidate } from '../interfaces/Candidate.interface';
+import "../styles/styles.css";
 
 const CandidateSearch = () => {
   const [users, setUsers] = useState<Candidate[]>([]);
+  const [user, setUser] = useState<Candidate | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     searchGithub().then((data) => {
-      setUsers(data.items);
+      setUsers(data);
+      searchUser(data[0]?.login);
     });
   }, []);
+
+
+  const searchUser = (username: string) => {
+    searchGithubUser(username).then((data) => {
+      setUser(data);
+    }
+    );
+  };
+
+  const nextUser = () => {
+    if (currentIndex === users.length - 1) {
+      setShowMessage(true);
+      return
+    }
+    if (currentIndex < users.length - 1) {
+      searchUser(users[currentIndex + 1]?.login);
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const saveUser = () => {
+    const savedUsers = JSON.parse(localStorage.getItem('savedUsers') || '[]');
+    savedUsers.push(user);
+    localStorage.setItem('savedUsers', JSON.stringify(savedUsers));
+    nextUser();
+  };
+
   return (
     <div>
       <h1>Candidate Search</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.login}>
-            <img src={user.avatar_url} alt={user.login} />
-            <a href={user.html_url}>{user.login}</a>
-            <p>{user.name}</p>
-            <p>{user.company}</p>
-            <p>{user.location}</p>
-            <p>{user.email}</p>
-            <p>{user.bio}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="card">
+        <img src={user?.avatar_url} alt={user?.login} className="avatar" />
+        <div className="card_body">
+          <h4>{user?.name}({user?.login})</h4>
+          <p>Location: {user?.location}</p>
+          <p>Email: <a href={`mailto:${user?.email}`}>{user?.email}</a></p>
+          <p>Company: {user?.company}</p>
+          <p>Bio: {user?.bio}</p>
+        </div>
+      </div>
+      <div className="button">
+        <button className="minusButton" onClick={nextUser}>-</button>
+        <button className="plusButton" onClick={saveUser}>+</button>
+      </div>
+      {showMessage && <p className="message">No more users to show</p>}
     </div>
   );
 };
